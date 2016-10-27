ac75990f130454aa4ff08070319668585b911082
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-10-27 01:29:43 -0500

    operations wrt numbers in es6

---
+++ 4_number.js
@@ -0,0 +1,10 @@
+var oct = 071;
+console.log(oct);
+
+// new features added in es6 to avoid confusion for octal & binary numbers. Add o/b respectively
+
+var octal = 0o71;
+console.log(octal);
+
+var bin = 0b0101;
+console.log(bin);
