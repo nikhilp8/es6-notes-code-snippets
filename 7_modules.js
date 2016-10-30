a133827a457969e5f76bff0d6bacbf9a6ea1de5e
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-10-29 23:17:56 -0500

    modules in JS

---
+++ 7_modules.js
@@ -0,0 +1,7 @@
+/*to hide implementation details and 
+to avoid global varaiables
+
+To place all related code in a single unit & for others to import this peace of code.
+
+should know the current state of packaging JS code
+*/
